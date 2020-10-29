9220a8547055ed32459420ed592839ec5f69a871
Aziz <ararwow@gmail.com>
2020-10-29 16:35:08 +0000

    fix: attempt google analytic fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
-
+import { useEffect } from 'react'
 import App from 'next/app'
+import { useRouter } from 'next/router'
 import UserContext from '../components/UserContext'
+import * as gtag from '../lib/gtag'
+
+function Analytics({ children }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <>{children}</>
+}
 
 export default class MyApp extends App {
   state = {
@@ -25,11 +42,13 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <UserContext.Provider
-        value={{ job: this.state.job, selectJob: this.selectJob, boss: this.state.boss, selectBoss: this.selectBoss }}
-      >
-        <Component {...pageProps} />
-      </UserContext.Provider>
+      <Analytics>
+        <UserContext.Provider
+          value={{ job: this.state.job, selectJob: this.selectJob, boss: this.state.boss, selectBoss: this.selectBoss }}
+        >
+          <Component {...pageProps} />
+        </UserContext.Provider>
+      </Analytics>
     )
   }
 }
